488443db0e065e0381342acff60a90d7ab081840
NurislamAituarov <nurislamajtuarov@gmail.com>
2023-11-21 13:16:19 +0600

    refactor style line hour

--- src/components/timeline/TimeLineHour.vue
+++ src/components/timeline/TimeLineHour.vue
@@ -1,17 +1,9 @@
 <template>
-  <a
-    href="#"
-    class="absolute -top-4 left-1/2 -translate-x-1/2 rounded px-2 font-mono text-lg"
-    :class="[
-      timelineItem.hour === new Date().getHours()
-        ? 'bg-purple-900 font-black text-white'
-        : 'bg-gray-100 text-gray-500',
-    ]"
-    >{{ formatterHour }}</a
-  >
-</template>
+  <a href="#" :class="classes">{{ formatterHour }}</a>
+</template> 
 
 <script lang="ts" setup>
+import { now } from "@/module/time";
 import { THourItem } from "@/types";
 import { computed } from "vue";
 
@@ -25,5 +17,14 @@ const formatterHour = computed(() => {
   const hour = props.timelineItem.hour;
 
   return `${hour.toString().padStart(2, "0")}:00`;
+});
+
+const classes = computed(() => {
+  return [
+    "absolute -top-4 left-1/2 -translate-x-1/2 rounded px-2 font-mono text-lg",
+    props.timelineItem.hour === now.value.getHours()
+      ? "bg-purple-900 font-black text-white"
+      : "bg-gray-100 text-gray-500",
+  ];
 });
 </script>
